23ddda5c3d08fb9b275cca444406d6559e5861af
Rohit-poddarr <poddarrohit301@gmail.com>
2025-05-09 21:25:42 +0530

    first commit

---
+++ task1.py
@@ -0,0 +1,17 @@
+
+number1 = input("Enter the first number: \n")
+number1 = int(number1)
+number2 = input("Enter the second number: \n")
+number2 = int(number2)
+
+
+Addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1*number2
+division = number1%number2
+division = float(division)
+
+print("Addition: ", Addition)
+print("subtraction: ", subtraction)
+print("multiplication" ,multiplication)
+print("Division: ", division)
