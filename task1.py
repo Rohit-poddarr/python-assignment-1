7ecc19156964bfaa0af833209c6614329f0a9652
Rohit-poddarr <poddarrohit301@gmail.com>
2025-05-09 23:15:20 +0530

    secon commmit

--- task1.py
+++ task1.py
@@ -8,10 +8,10 @@ number2 = int(number2)
 Addition = number1 + number2
 subtraction = number1 - number2
 multiplication = number1*number2
-division = number1%number2
+division = number1/number2
 division = float(division)
 
-print("Addition: ", Addition)
+print("Addition: ", Addition)   
 print("subtraction: ", subtraction)
 print("multiplication" ,multiplication)
 print("Division: ", division)
